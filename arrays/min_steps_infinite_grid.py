4219c16e7d8312c437bcc19e282be94419102523
Srikanth Reddy Metlakunta <srikanth_m07@infosys.com>
2018-02-11 12:06:32 +0530

    min steps array

--- arrays/min_steps_infinite_grid.py
+++ arrays/min_steps_infinite_grid.py
@@ -1,16 +1,21 @@
-import math
 
-def max_steps_infinite(A, B):
-    if len(A) == 0 and len(A) != len(B):
-        return 0
-    value = 0
-    for i in range(0, len(A)):
-        value += max(abs(A[i] - A[i-1]), abs(B[i] - B[i-1]))
-        return value
-
-
-
+def min_steps_infinite(A, A_n, B, B_n):
+    prev_x, prev_y, total = A[0], B[0], 0
+    for curr_x, curr_y in zip(A, B):
+        dx, dy = abs(curr_x - prev_x), abs(curr_y - prev_y)
+        if dx < dy:
+            total += dy
+        else:
+            total += dx
+        prev_x, prev_y = curr_x, curr_y
+    return total
+    
 #A = [0,1,1]
 #B = [0,1,2]
 #A = [ -7, -13 ]
 #B = [ 1, -5 ]
+A = [ -5, 7, -12, 4, -6, 2, -5, -12, -6, 3, 11, 10, -8, 11, -13, -8, 5, -12, 4, 4 ]
+B = [ -6, 6, -8, -13, -2, -9, -10, -10, -7, -14, 9, -8, -4, 8, 13, -11, 13, 5, 9, 11 ]
+A_n = len(A)
+B_n = len(B)
+print('no of steps are:-', min_steps_infinite(A,A_n, B, B_n))
