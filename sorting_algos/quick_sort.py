577d6a5745397f5975721667bace975ed2a7e67c
Srikanth Reddy Metlakunta <srikanth_m07@infosys.com>
2018-02-25 12:37:22 +0530

    quick sort

---
+++ sorting_algos/quick_sort.py
@@ -0,0 +1,30 @@
+    
+def partition(A, start, end):
+    pivot = A[end]
+    pIndex = start
+    for i in range(start, end):
+        if A[i] <= pivot:
+            tmp = A[i]
+            A[i] = A[pIndex]
+            A[pIndex] = tmp
+            pIndex = pIndex + 1
+            
+    tmp = A[pIndex]
+    A[pIndex] = A[end]
+    A[end] = tmp
+    
+    return pIndex
+
+def quick_sorting(A, start, end):
+    pivot = A[end]
+    if start < end:
+        pIndex = partition(A, start, end)
+        quick_sorting(A, start, pIndex-1)
+        quick_sorting(A, pIndex+1, end)
+    return A
+
+
+A = [7, 2, 1, 6, 8, 5, 3, 4]
+end = len(A)-1
+start = 0
+print('quick sorted list is :- ', quick_sorting(A, start, end))
