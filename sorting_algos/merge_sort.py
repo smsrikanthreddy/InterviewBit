4d2f2e40ca67a93fa4e1fc72ab3b49e9b2e610c6
smsrikanthreddy <smsrikanthreddy@gmail.com>
2018-03-06 07:46:20 +0530

    merge sort

---
+++ sorting_algos/merge_sort.py
@@ -0,0 +1,38 @@
+
+def merge(L, R, A):
+    nL = len(L)
+    nR = len(R)
+    k, j, i = 0,0,0
+    while i<nL and j <nR :
+        if L[i] <= R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+        k += 1
+    while i<nL:
+        A[k] = L[i]
+        i += 1
+        k += 1
+    while j<nR:
+        A[k] = R[j]
+        j += 1
+        k += 1
+    return A
+
+
+def mergesort(A):
+    l_n = len(A)
+    if l_n < 2:
+        return A
+    mid = int(l_n/2)
+    L = A[:mid]
+    R = A[mid:]
+    mergesort(L)
+    mergesort(R)
+    merge(L, R, A)
+    return A
+
+A = [23, 32, 4, 5, 6, 76]
+print('merge sort sorted list is :-', mergesort(A))
