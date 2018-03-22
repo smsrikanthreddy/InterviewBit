13fef17f6de32d6c43fec9291d6682a6d9a77a7e
smsrikanthreddy <smsrikanthreddy@gmail.com>
2018-03-22 23:37:45 +0530

    redundant braces

--- stacks_and_queues/redundant_braces.py
+++ stacks_and_queues/redundant_braces.py
@@ -1,2 +1,16 @@
-def redundant_braces():
-    
+class Solution:
+    	# @param A : string
+	# @return an integer
+    def braces(self, A):
+        braces = 0
+        for char in A :
+            if char == '(' :
+                braces += 1
+            elif char in "*/+-" :
+                braces -= 1
+            if braces < 0 :
+                braces = 0
+        if braces == 0 :
+            return 0
+        else :
+            return 1    
