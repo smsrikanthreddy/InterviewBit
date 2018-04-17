10212acfe4bf40c6e30f5f5fcb291687fde70685
smsrikanthreddy <smsrikanthreddy@gmail.com>
2018-04-17 15:54:29 +0530

    rearrange array

---
+++ maths/rearrange_array.py
@@ -0,0 +1,18 @@
+
+def rearrange_array(A):
+    n = len(A)
+    for i in range(n):
+        A[i] += (A[A[i]]%n)*n
+    print('1-step array is:-', A)
+    for i in range(n):
+        A[i] = A[i]//n
+
+    return A
+
+
+
+
+
+A = [3, 2, 0, 1]
+#A = [4,2,0,1,3]
+print('rearranged array is:-', rearrange_array(A))
